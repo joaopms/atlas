d7eaecfb85958ffacdeca960d3ae0d523374d444
João Silva <git@joaopms.pt>
2026-08-09 17:49:55 +0100

    Allow trusting all proxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,11 +13,13 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->trustProxies(
-            at: [
-                ...array_filter(
-                    explode(',', (string) env('TRUSTED_PROXIES', ''))
-                ),
-            ],
+            at: (string) env('TRUSTED_PROXIES', '') === '*'
+                ? '*'
+                : [
+                    ...array_filter(
+                        explode(',', (string) env('TRUSTED_PROXIES', ''))
+                    ),
+                ],
             headers: Request::HEADER_X_FORWARDED_FOR |
             Request::HEADER_X_FORWARDED_HOST |
             Request::HEADER_X_FORWARDED_PORT |
